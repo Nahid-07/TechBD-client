d8aff599311ed8495c37680ca66a23f3867be406
Nahid-07 <nahidasif1998@gmail.com>
2022-10-27 14:09:05 +0600

    readme file added

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Components/Footer';
-import Home from '../Components/Home';
 import Navbar from '../Components/Navbar';
 
 const Layout = () => {
